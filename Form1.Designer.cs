99a140e399fc03244417e32069aa355b78b07e49
Tristan Emmerson <tristan@stickerslug.com>
2021-01-10 12:30:12 -0800

    add title and body label tags

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,13 @@ namespace CodeChallenge
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnClickThis = new System.Windows.Forms.Button();
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(26, 300);
+            this.richTextBox1.Location = new System.Drawing.Point(397, 216);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(687, 325);
             this.richTextBox1.TabIndex = 0;
@@ -45,14 +47,14 @@ namespace CodeChallenge
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 190);
+            this.textBox1.Location = new System.Drawing.Point(397, 118);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(687, 27);
             this.textBox1.TabIndex = 1;
             // 
             // btnClickThis
             // 
-            this.btnClickThis.Location = new System.Drawing.Point(753, 189);
+            this.btnClickThis.Location = new System.Drawing.Point(1189, 271);
             this.btnClickThis.Name = "btnClickThis";
             this.btnClickThis.Size = new System.Drawing.Size(94, 29);
             this.btnClickThis.TabIndex = 3;
@@ -69,17 +71,37 @@ namespace CodeChallenge
             this.lblHelloWorld.TabIndex = 4;
             this.lblHelloWorld.Click += new System.EventHandler(this.lblHelloWorld_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Body";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2121, 1453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.btnClickThis);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Code Challenge Form";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -92,6 +114,8 @@ namespace CodeChallenge
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnClickThis;
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
